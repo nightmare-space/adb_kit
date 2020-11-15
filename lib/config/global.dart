aee0ae0a8eb041d5c3b3e34f7ebdcf6838e1b8d6
Nightmare-MY <906262255@qq.com>
2020-11-15 14:41:21 +0800

    modify global class

--- lib/config/global.dart
+++ lib/config/global.dart
@@ -3,19 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:global_repository/global_repository.dart';
 
 class Global {
-  // 工厂模式
   factory Global() => _getInstance();
   Global._internal() {
     environment = PlatformUtil.environment();
     themeFollowSystem = true;
   }
   GlobalKey<NavigatorState> navigatorKey = GlobalKey();
-  // 由于使用的嘶页面级别的Provider，所以push后的context会找不到Provider的祖先节点
+  // 由于使用的是页面级别的Provider，所以push后的context会找不到Provider的祖先节点
   ProcessState processState;
   Map<String, String> environment;
-  // 用户信息
-  // 主题状态
-  ThemeMode themeMode = ThemeMode.light;
   bool themeFollowSystem;
   String _documentsDir;
   static Global get instance => _getInstance();
