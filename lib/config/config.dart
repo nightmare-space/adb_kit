1f722c02c69392a71f1ccf167d252dbd5da6c1e3
Nightmare-MY <906262255@qq.com>
2020-11-18 22:06:34 +0800

    Update config.dart

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,10 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:global_repository/global_repository.dart';
 
 class Config {
   Config._();
-  static String baseURL =
-      kReleaseMode ? 'https://api2.bmob.cn/1' : 'https://api2.bmob.cn/1';
 
   static String dbPath = '/data/data/com.nightmare/databases/user.db';
   static String packageName = 'com.nightmare.adbtool';
