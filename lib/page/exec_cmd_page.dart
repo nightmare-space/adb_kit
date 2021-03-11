f1147cd357f570a753ad2981266ef93bfa11bc3c
Nightmare-MY <906262255@qq.com>
2021-03-11 16:30:42 +0800

    support pty term

--- lib/page/exec_cmd_page.dart
+++ lib/page/exec_cmd_page.dart
@@ -2,10 +2,12 @@ import 'dart:io';
 
 import 'package:adb_tool/config/dimens.dart';
 import 'package:adb_tool/global/provider/process_info.dart';
+import 'package:dart_pty/dart_pty.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:global_repository/global_repository.dart';
 import 'package:provider/provider.dart';
+import 'package:termare_pty/termare_pty.dart';
 
 import 'process_page.dart';
 
@@ -16,26 +18,32 @@ class ExecCmdPage extends StatefulWidget {
 
 class _ExecCmdPageState extends State<ExecCmdPage> {
   TextEditingController editingController = TextEditingController();
+  PseudoTerminal pseudoTerminal = PseudoTerminal(
+    executable: 'sh',
+    environment: PlatformUtil.environment(),
+  );
   FocusNode focusNode = FocusNode();
   Future<void> execCmd() async {
     // Provider.of<ProcessState>(context).clear();
     // final String cmd = editingController.text;
     // final String result = await exec('echo $cmd\n$cmd');
     // Provider.of<ProcessState>(context).appendOut(result);
-    Provider.of<ProcessState>(context).clear();
-    NiProcess.exec(
-      editingController.text,
-      getStderr: true,
-      callback: (output) {
-        print('ss======>$output');
 
-        if (output.trim() == 'process_exit') {
-          return;
-        }
-        output = output.replaceAll('process_exit', '');
-        Provider.of<ProcessState>(context).appendOut(output);
-      },
-    );
+    // Provider.of<ProcessState>(context).clear();
+    pseudoTerminal.write(editingController.text + '\n');
+    // NiProcess.exec(
+    //   editingController.text,
+    //   getStderr: true,
+    //   callback: (output) {
+    //     print('ss======>$output');
+
+    //     if (output.trim() == 'process_exit') {
+    //       return;
+    //     }
+    //     output = output.replaceAll('process_exit', '');
+    //     Provider.of<ProcessState>(context).appendOut(output);
+    //   },
+    // );
     editingController.clear();
     focusNode.requestFocus();
   }
@@ -47,100 +55,117 @@ class _ExecCmdPageState extends State<ExecCmdPage> {
       appBar = AppBar(
         brightness: Brightness.light,
         title: const Text('执行命令'),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+        ),
       );
     }
     return Scaffold(
       // backgroundColor: Colors.white,
       appBar: appBar,
-      body: Stack(
-        alignment: Alignment.topCenter,
+      body: Column(
         children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(
-              Dimens.gap_dp8,
-              Dimens.gap_dp8,
-              Dimens.gap_dp8,
-              Dimens.gap_dp8,
-            ),
-            child: Stack(
-              children: [
-                ConstrainedBox(
-                  constraints: BoxConstraints(
-                    maxHeight: MediaQuery.of(context).size.height -
-                        kToolbarHeight -
-                        Dimens.setWidth(140),
-                  ),
-                  // height: MediaQuery.of(context).size.height * 3 / 4,
-                  child: const ProcessPage(),
-                ),
-              ],
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: SingleChildScrollView(
-              child: Material(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(
-                    Dimens.gap_dp8,
-                    Dimens.gap_dp8,
-                    Dimens.gap_dp8,
-                    Dimens.gap_dp12,
-                  ),
-                  child: SizedBox(
-                    height: Dimens.setWidth(72),
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(Dimens.gap_dp12),
-                          child: TextField(
-                            focusNode: focusNode,
-                            controller: editingController,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                            decoration: InputDecoration(
-                              // border: OutlineInputBorder(
-                              //   borderRadius: BorderRadius.circular(Dimens.gap_dp8),
-                              // ),
-                              border: InputBorder.none,
-                              filled: true,
-                              fillColor: Color(0xFFF0F0F0),
-                            ),
-                            onSubmitted: (_) {
-                              execCmd();
-                            },
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: SizedBox(
-                            height: Dimens.setWidth(72),
-                            width: Dimens.setWidth(72),
-                            child: Material(
-                              color: Colors.transparent,
-                              child: IconButton(
-                                icon: Icon(
-                                  Icons.arrow_forward_ios,
-                                  color: Colors.black.withOpacity(0.6),
-                                ),
-                                onPressed: () async {
-                                  execCmd();
-                                },
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+          Expanded(
+            child: TermarePty(
+              pseudoTerminal: pseudoTerminal,
             ),
           ),
         ],
       ),
+      // body: Stack(
+      //   alignment: Alignment.topCenter,
+      //   children: [
+      //     Padding(
+      //       padding: EdgeInsets.fromLTRB(
+      //         Dimens.gap_dp8,
+      //         Dimens.gap_dp8,
+      //         Dimens.gap_dp8,
+      //         Dimens.gap_dp8,
+      //       ),
+      //       child: Stack(
+      //         children: [
+      //           ConstrainedBox(
+      //             constraints: BoxConstraints(
+      //               maxHeight: MediaQuery.of(context).size.height -
+      //                   kToolbarHeight -
+      //                   Dimens.setWidth(140),
+      //             ),
+      //             // height: MediaQuery.of(context).size.height * 3 / 4,
+      //             child: TermarePty(
+      //               pseudoTerminal: pseudoTerminal,
+      //             ),
+      //           ),
+      //         ],
+      //       ),
+      //     ),
+      //     Align(
+      //       alignment: Alignment.bottomCenter,
+      //       child: SingleChildScrollView(
+      //         child: Material(
+      //           child: Padding(
+      //             padding: EdgeInsets.fromLTRB(
+      //               Dimens.gap_dp8,
+      //               Dimens.gap_dp8,
+      //               Dimens.gap_dp8,
+      //               Dimens.gap_dp12,
+      //             ),
+      //             child: SizedBox(
+      //               height: Dimens.setWidth(72),
+      //               child: Stack(
+      //                 alignment: Alignment.center,
+      //                 children: [
+      //                   ClipRRect(
+      //                     borderRadius: BorderRadius.circular(Dimens.gap_dp12),
+      //                     child: TextField(
+      //                       focusNode: focusNode,
+      //                       controller: editingController,
+      //                       style: const TextStyle(
+      //                         fontWeight: FontWeight.bold,
+      //                       ),
+      //                       decoration: InputDecoration(
+      //                         // border: OutlineInputBorder(
+      //                         //   borderRadius: BorderRadius.circular(Dimens.gap_dp8),
+      //                         // ),
+      //                         border: InputBorder.none,
+      //                         filled: true,
+      //                         fillColor: Color(0xFFF0F0F0),
+      //                       ),
+      //                       onSubmitted: (_) {
+      //                         execCmd();
+      //                       },
+      //                     ),
+      //                   ),
+      //                   Align(
+      //                     alignment: Alignment.centerRight,
+      //                     child: SizedBox(
+      //                       height: Dimens.setWidth(72),
+      //                       width: Dimens.setWidth(72),
+      //                       child: Material(
+      //                         color: Colors.transparent,
+      //                         child: IconButton(
+      //                           icon: Icon(
+      //                             Icons.arrow_forward_ios,
+      //                             color: Colors.black.withOpacity(0.6),
+      //                           ),
+      //                           onPressed: () async {
+      //                             execCmd();
+      //                           },
+      //                         ),
+      //                       ),
+      //                     ),
+      //                   ),
+      //                 ],
+      //               ),
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
