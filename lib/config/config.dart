eec3f4b888fb5bdcbb856bad70e462f534a2b1cf
Nightmare-MY <906262255@qq.com>
2021-03-22 13:33:04 +0800

    upgrade app version

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -5,7 +5,7 @@ class Config {
   static Map<String, String> devicesMap = {};
   static String historyIp = '';
   static bool conWhenScan = true;
-  static String version = '1.0.5';
+  static String version = '1.0.6';
   // 224.0.0.1 这个组播ip可以实现手机热点电脑，电脑发送组播，手机接收到
   // static InternetAddress multicastAddress = InternetAddress('224.0.0.1');
 }
