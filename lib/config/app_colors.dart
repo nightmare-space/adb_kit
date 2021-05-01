6328ce36d2db69fc43d613039a51f4a849869647
Nightmare-MY <906262255@qq.com>
2021-05-01 22:19:03 +0800

    Update app_colors.dart

--- lib/config/app_colors.dart
+++ lib/config/app_colors.dart
@@ -1,7 +1,12 @@
 import 'dart:ui';
 
+import 'package:adb_tool/config/candy_colors.dart';
+
 class AppColors {
   static Color fontTitle = Color(0xff0d0d14);
 
+  // static Color accent = const Color(0xff3573e0);
+  static Color accent = CandyColors.indigo;
   static Color fontDetail = Color(0xff707076);
+  static Color contentBorder = Color(0xfff7f7f8);
 }
