6530fc234517a04e9d0e73710d8fc4b7cefef10f
Nightmare-MY <906262255@qq.com>
2021-05-01 22:19:50 +0800

    update config file

--- scripts/generate_mac_app.sh
+++ scripts/generate_mac_app.sh
@@ -1,7 +1,7 @@
 flutter build macos
 rm -rf ./build/macos/Build/Products/Release/移动到这
 ln -s -f /Applications ./build/macos/Build/Products/Release/移动到这
-mkdir -p ./build/macos/Build/Products/Release/adb_tool.app/Contents/MacOS/data/usr/bin
-cp ./executable/macos/adb ./build/macos/Build/Products/Release/adb_tool.app/Contents/MacOS/data/usr/bin
-tar -zcvf ./remote.tar  -C ./build/macos/Build/Products/  Release/adb_tool.app/ Release/移动到这
+mkdir -p "./build/macos/Build/Products/Release/ADB TOOL.app/Contents/MacOS/data/usr/bin"
+cp ./executable/macos/adb "./build/macos/Build/Products/Release/ADB TOOL.app/Contents/MacOS/data/usr/bin"
+tar -zcvf ./ADBTOOL.tar  -C ./build/macos/Build/Products/  "Release/ADB TOOL.app/" Release/移动到这
 rm -rf ./build/macos/Build/Products/Release/移动到这
