12be17f037cb9492efa5d6129536dd6b8d7533f4
Nightmare-MY <mengyanshou@gmail.com>
2021-05-31 15:25:00 +0800

    add some color

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,31 @@
+import 'dart:ui';
+
+import 'candy_colors.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color fontColor = Color(0xff201b1a);
+
+  // static const Color fontColor = Color(0xff355c7d);
+  static const Color inputBorderColor = Color(0xffeeeeee);
+  // static const Color accentColor = Color(0xff811016);
+  // dribble
+  // static const Color accentColor = Color(0xffd85988);
+
+  // static const Color accentColor = Color(0xfff67280);
+  // 深紫红
+  // static const Color accentColor = Color(0xff480032);
+  // 红，挺好看
+  //
+  // static const Color accentColor = Color(0xffb9135d);
+
+  // static const Color accentColor = Color(0xff867ae8);
+  // static const Color accentColor = Color(0xffe23e57);
+  static Color fontTitle = const Color(0xff0d0d14);
+  static Color fontDetail = Color(0xff707076);
+  static const Color background =
+      Color(0xfff5f5f7); // static Color accent = const Color(0xff3573e0);
+  static const Color accent = CandyColors.indigo;
+  static Color contentBorder = const Color(0xfff7f7f8);
+  static Color terminalBack = const Color(0xffeeeeee);
+}
