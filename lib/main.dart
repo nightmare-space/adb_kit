b6035f56700c2b1ae1f589c48c7c7583c602f8e2
Nightmare-MY <mengyanshou@gmail.com>
2021-06-04 16:52:00 +0800

    add test code

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 library adb_tool;
 
 import 'dart:io';
-import 'package:signale/signale.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:global_repository/global_repository.dart';
+import 'package:multicast_dns/multicast_dns.dart';
+import 'package:signale/signale.dart';
+
 import 'app/routes/app_pages.dart';
 import 'config/config.dart';
 import 'global/instance/global.dart';
@@ -21,6 +23,7 @@ void main() {
 
   runApp(
     GetMaterialApp(
+      enableLog: false,
       title: 'ADB TOOL',
       initialRoute: AdbPages.INITIAL,
       getPages: AdbPages.routes,
@@ -47,4 +50,31 @@ Future<void> enableUdpMulti() async {
     final bool result = await methodChannel.invokeMethod<bool>('aquire');
     Log.d('result -> $result');
   }
+  return;
+  const String name = '_http._tcp';
+  final MDnsClient client = MDnsClient(rawDatagramSocketFactory:
+      (dynamic host, int port, {bool reuseAddress, bool reusePort, int ttl}) {
+    return RawDatagramSocket.bind(host, port,
+        reuseAddress: true, reusePort: false, ttl: ttl);
+  });
+  // Start the client with default options.
+  await client.start();
+
+  // Get the PTR record for the service.
+  await for (final PtrResourceRecord ptr in client
+      .lookup<PtrResourceRecord>(ResourceRecordQuery.serverPointer(name))) {
+    // Use the domainName from the PTR record to get the SRV record,
+    // which will have the port and local hostname.
+    // Note that duplicate messages may come through, especially if any
+    // other mDNS queries are running elsewhere on the machine.
+    await for (final SrvResourceRecord srv in client.lookup<SrvResourceRecord>(
+        ResourceRecordQuery.service(ptr.domainName))) {
+      // Domain name will be something like "io.flutter.example@some-iphone.local._dartobservatory._tcp.local"
+      final String bundleId =
+          ptr.domainName; //.substring(0, ptr.domainName.indexOf('@'));
+      print('Dart observatory instance found at '
+          '${srv.target}:${srv.port} for "$bundleId".');
+    }
+  }
+  // client.stop();
 }
