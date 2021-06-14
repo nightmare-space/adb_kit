d72b496d754ce6f81e25339fae10eadcc0e6616b
Nightmare-MY <906262255@qq.com>
2021-06-14 11:28:44 +0800

    update shell

--- scripts/generate_mac_app.sh
+++ scripts/generate_mac_app.sh
@@ -3,5 +3,5 @@ rm -rf ./build/macos/Build/Products/Release/移动到这
 ln -s -f /Applications ./build/macos/Build/Products/Release/移动到这
 mkdir -p "./build/macos/Build/Products/Release/ADB TOOL.app/Contents/MacOS/data/usr/bin"
 cp ./executable/macos/adb "./build/macos/Build/Products/Release/ADB TOOL.app/Contents/MacOS/data/usr/bin"
-tar -zcvf ./ADBTOOL.tar  -C ./build/macos/Build/Products/  "Release/ADB TOOL.app/" Release/移动到这
+tar -zcvf ./AdbTool_macOS.tar  -C ./build/macos/Build/Products/  "Release/ADB TOOL.app/" Release/移动到这
 rm -rf ./build/macos/Build/Products/Release/移动到这
