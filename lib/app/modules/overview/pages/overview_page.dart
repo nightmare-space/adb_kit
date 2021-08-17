92fa25820358ecaf07826478c3e67c75119118ad
Nightmare <mengyanshou@gmail.com>
2021-08-17 19:20:17 +0800

    adjust ui

--- lib/app/modules/overview/pages/overview_page.dart
+++ lib/app/modules/overview/pages/overview_page.dart
@@ -6,9 +6,7 @@ import 'package:adb_tool/app/modules/online_devices/views/online_view.dart';
 import 'package:adb_tool/app/modules/overview/list/devices_list.dart';
 import 'package:adb_tool/global/instance/global.dart';
 import 'package:adb_tool/global/pages/terminal.dart';
-import 'package:adb_tool/global/widget/custom_icon_button.dart';
 import 'package:adb_tool/global/widget/item_header.dart';
-import 'package:adb_tool/themes/app_colors.dart';
 import 'package:adb_tool/utils/scan_util.dart';
 import 'package:adbutil/adbutil.dart';
 import 'package:flutter/foundation.dart';
@@ -94,170 +92,173 @@ class _OverviewPageState extends State<OverviewPage> {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 8.w),
       child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // MacosContextMenuItem(
-            //   content: Text('data'),
-            // ),
-            Row(
-              children: [
-                const ItemHeader(color: CandyColors.candyPink),
-                Text(
-                  '已成功连接的设备',
-                  style: TextStyle(
-                    fontSize: Dimens.font_sp16,
-                    fontWeight: FontWeight.bold,
-                    height: 1.0,
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // MacosContextMenuItem(
+              //   content: Text('data'),
+              // ),
+              Row(
+                children: [
+                  const ItemHeader(color: CandyColors.candyPink),
+                  Text(
+                    '已成功连接的设备',
+                    style: TextStyle(
+                      fontSize: Dimens.font_sp16,
+                      fontWeight: FontWeight.bold,
+                      height: 1.0,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: Dimens.gap_dp8,
+                ],
               ),
-              child: DevicesList(),
-            ),
-            SizedBox(height: 12.w),
-            Row(
-              children: [
-                const ItemHeader(color: CandyColors.candyBlue),
-                Text(
-                  '快捷命令',
-                  style: TextStyle(
-                    fontSize: Dimens.font_sp16,
-                    fontWeight: FontWeight.bold,
-                    height: 1.0,
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: Dimens.gap_dp8,
+                ),
+                child: DevicesList(),
+              ),
+              SizedBox(height: 12.w),
+              Row(
+                children: [
+                  const ItemHeader(color: CandyColors.candyBlue),
+                  Text(
+                    '快捷命令',
+                    style: TextStyle(
+                      fontSize: Dimens.font_sp16,
+                      fontWeight: FontWeight.bold,
+                      height: 1.0,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Wrap(
-              spacing: 0,
-              runSpacing: 0,
-              alignment: WrapAlignment.start,
-              runAlignment: WrapAlignment.start,
-              crossAxisAlignment: WrapCrossAlignment.start,
-              children: [
-                ItemButton(
-                  title: '开启服务',
-                  onTap: () async {
-                    const String cmd = 'adb start-server\r';
-                    Global.instance.pseudoTerminal.write(cmd);
-                    AdbUtil.startPoolingListDevices();
-                  },
-                ),
-                ItemButton(
-                  title: '停止服务',
-                  onTap: () async {
-                    const String cmd = 'adb kill-server\r';
-                    Global.instance.pseudoTerminal.write(cmd);
-                    AdbUtil.stopPoolingListDevices();
-                    DevicesController controller = Get.find();
-                    controller.clearDevices();
-                  },
-                ),
-                ItemButton(
-                  title: '重启服务',
-                  onTap: () async {
-                    const String cmd = 'adb kill-server && adb start-server\r';
-                    Global.instance.pseudoTerminal.write(cmd);
-                  },
-                ),
-                ItemButton(
-                  title: '连接远程设备',
-                  onTap: () async {
-                    final String cmd = await showCustomDialog<String>(
-                      context: context,
-                      child: Theme(
-                        data: Theme.of(context),
-                        child: ConnectRemote(),
-                      ),
-                    );
-                    if (cmd == null) {
-                      return;
-                    }
-                    Global.instance.pseudoTerminal.write(cmd);
-                  },
-                ),
-                ItemButton(
-                  title: '连接二维码',
-                  onTap: () async {
-                    await showDialog<String>(
-                      context: context,
-                      builder: (_) {
-                        return QrScanPage();
-                      },
-                    );
-                  },
-                ),
-                ItemButton(
-                  title: '复制ADB KEY',
-                  onTap: () async {
-                    String homePath = '';
-                    if (Platform.isMacOS) {
-                      homePath = Platform.environment['HOME'];
-                    } else if (Platform.isAndroid) {
-                      homePath = RuntimeEnvir.binPath;
-                    }
-                    final File adbKey = File(
-                      '$homePath/.android/adbkey.pub',
-                    );
-                    if (adbKey.existsSync()) {
-                      await Clipboard.setData(
-                        ClipboardData(
-                          text: adbKey.readAsStringSync(),
+                ],
+              ),
+              Wrap(
+                spacing: 0,
+                runSpacing: 0,
+                alignment: WrapAlignment.start,
+                runAlignment: WrapAlignment.start,
+                crossAxisAlignment: WrapCrossAlignment.start,
+                children: [
+                  ItemButton(
+                    title: '开启服务',
+                    onTap: () async {
+                      const String cmd = 'adb start-server\r';
+                      Global.instance.pseudoTerminal.write(cmd);
+                      AdbUtil.startPoolingListDevices();
+                    },
+                  ),
+                  ItemButton(
+                    title: '停止服务',
+                    onTap: () async {
+                      const String cmd = 'adb kill-server\r';
+                      Global.instance.pseudoTerminal.write(cmd);
+                      AdbUtil.stopPoolingListDevices();
+                      DevicesController controller = Get.find();
+                      controller.clearDevices();
+                    },
+                  ),
+                  ItemButton(
+                    title: '重启服务',
+                    onTap: () async {
+                      const String cmd =
+                          'adb kill-server && adb start-server\r';
+                      Global.instance.pseudoTerminal.write(cmd);
+                    },
+                  ),
+                  ItemButton(
+                    title: '连接远程设备',
+                    onTap: () async {
+                      final String cmd = await showCustomDialog<String>(
+                        context: context,
+                        child: Theme(
+                          data: Theme.of(context),
+                          child: ConnectRemote(),
                         ),
                       );
-                      showToast('已复制');
-                    } else {
-                      showToast('未发现adb key');
-                    }
-                  },
-                ),
-              ],
-            ),
-            SizedBox(height: 12.w),
-            Row(
-              children: [
-                const ItemHeader(color: CandyColors.candyPurpleAccent),
-                Text(
-                  '运行ADB TOOL的设备',
-                  style: TextStyle(
-                    fontSize: Dimens.font_sp16,
-                    fontWeight: FontWeight.bold,
-                    height: 1.0,
+                      if (cmd == null) {
+                        return;
+                      }
+                      Global.instance.pseudoTerminal.write(cmd);
+                    },
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: Dimens.gap_dp8,
-            ),
-            OnlineView(),
-            SizedBox(height: 12.w),
-            Row(
-              children: [
-                const ItemHeader(color: CandyColors.candyCyan),
-                Text(
-                  '终端',
-                  style: TextStyle(
-                    fontSize: Dimens.font_sp16,
-                    fontWeight: FontWeight.bold,
+                  ItemButton(
+                    title: '连接二维码',
+                    onTap: () async {
+                      await showDialog<String>(
+                        context: context,
+                        builder: (_) {
+                          return QrScanPage();
+                        },
+                      );
+                    },
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 12.w),
-            const SizedBox(
-              height: 200,
-              child: TerminalPage(),
-            ),
-            // ProcessPage(
-            //   height: 100,
-            // ),
-          ],
+                  ItemButton(
+                    title: '复制ADB KEY',
+                    onTap: () async {
+                      String homePath = '';
+                      if (Platform.isMacOS) {
+                        homePath = Platform.environment['HOME'];
+                      } else if (Platform.isAndroid) {
+                        homePath = RuntimeEnvir.binPath;
+                      }
+                      final File adbKey = File(
+                        '$homePath/.android/adbkey.pub',
+                      );
+                      if (adbKey.existsSync()) {
+                        await Clipboard.setData(
+                          ClipboardData(
+                            text: adbKey.readAsStringSync(),
+                          ),
+                        );
+                        showToast('已复制');
+                      } else {
+                        showToast('未发现adb key');
+                      }
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 12.w),
+              Row(
+                children: [
+                  const ItemHeader(color: CandyColors.candyPurpleAccent),
+                  Text(
+                    '运行ADB TOOL的设备',
+                    style: TextStyle(
+                      fontSize: Dimens.font_sp16,
+                      fontWeight: FontWeight.bold,
+                      height: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: Dimens.gap_dp8,
+              ),
+              OnlineView(),
+              SizedBox(height: 12.w),
+              Row(
+                children: [
+                  const ItemHeader(color: CandyColors.candyCyan),
+                  Text(
+                    '终端',
+                    style: TextStyle(
+                      fontSize: Dimens.font_sp16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 12.w),
+              const SizedBox(
+                height: 200,
+                child: TerminalPage(),
+              ),
+              // ProcessPage(
+              //   height: 100,
+              // ),
+            ],
+          ),
         ),
       ),
     );
