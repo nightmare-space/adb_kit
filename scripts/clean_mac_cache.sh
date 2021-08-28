9d805e25dfbc6cbffe598bd866eb40bf37bda4bc
Nightmare <mengyanshou@gmail.com>
2021-08-28 21:00:07 +0800

    add script

--- scripts/clean_mac_cache.sh
+++ scripts/clean_mac_cache.sh
@@ -1,1 +1,2 @@
 rm -rf ./build/macos/Build/Products/Debug/*
+rm -rf ./build/macos/Build/Products/Release/*
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,6 +1,7 @@
-version='1.1.2'
+version='1.1.3'
+app_name='AdbTool'
 target=root@$server:/home/nightmare/YanTool/resources/AdbTool
-if [ -f "AdbTool_macOS.tar" ]; then
-    rsync -v "AdbTool_macOS.tar" "$target/AdbTool_$version\_macOS.tar"
+if [ -f "$app_name\_macOS.tar" ]; then
+    rsync -v "$app_name\_macOS.tar" "$target/$app_name\_$version\_macOS.tar"
 fi
-rsync -v build/app/outputs/apk/release/app-release.apk "$target/AdbTool_$version\_Android_arm64.apk"
+rsync -v build/app/outputs/apk/release/app-release.apk "$target/$app_name\_$version\_Android_arm64.apk"
