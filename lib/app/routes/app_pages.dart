7918bcfbc5a9caa7a46b63fcad54024e47f8f69f
Nightmare <mengyanshou@gmail.com>
2021-08-28 21:01:21 +0800

    add drawer route

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,14 @@
+import 'package:adb_tool/app/modules/about_page.dart';
+import 'package:adb_tool/app/modules/drawer/log_page.dart';
+import 'package:adb_tool/app/modules/exec_cmd_page.dart';
+import 'package:adb_tool/app/modules/history/history_page.dart';
 import 'package:adb_tool/app/modules/home/bindings/home_binding.dart';
 import 'package:adb_tool/app/modules/home/views/home_view.dart';
+import 'package:adb_tool/app/modules/install/adb_insys_page.dart';
+import 'package:adb_tool/app/modules/net_debug/remote_debug_page.dart';
+import 'package:adb_tool/app/modules/overview/pages/overview_page.dart';
+import 'package:adb_tool/app/modules/search_ip_page.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 part 'app_routes.dart';
@@ -15,4 +24,35 @@ class AdbPages {
       binding: HomeBinding(),
     ),
   ];
+}
+
+Widget getWidget(String route) {
+  switch (route) {
+    case Routes.about:
+      return AboutPage();
+      break;
+    case Routes.overview:
+      return OverviewPage();
+      break;
+    case Routes.installToSystem:
+      return AdbInstallToSystemPage();
+      break;
+    case Routes.terminal:
+      return ExecCmdPage();
+      break;
+    case Routes.history:
+      return HistoryPage();
+      break;
+    case Routes.searchIp:
+      return SearchIpPage();
+      break;
+    case Routes.netDebug:
+      return RemoteDebugPage();
+      break;
+    case Routes.log:
+      return LogPage();
+      break;
+    default:
+    return SizedBox();
+  }
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,5 +3,13 @@ part of 'app_pages.dart';
 
 abstract class Routes {
   static const prefix = '/adb';
-  static const HOME = '$prefix/home';
+  static const HOME = prefix;
+  static const overview = '$prefix/overview';
+  static const installToSystem = '$prefix/install';
+  static const searchIp = '$prefix/search';
+  static const netDebug = '$prefix/debug';
+  static const terminal = '$prefix/terminal';
+  static const history = '$prefix/history';
+  static const about = '$prefix/about';
+  static const log = '$prefix/log';
 }
