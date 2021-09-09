58ef1f5a43fd90e4bdd6ff6f5fa5da08350434eb
Nightmare <mengyanshou@gmail.com>
2021-09-09 18:42:10 +0800

    update docs

--- lib/utils/assets_utils.dart
+++ lib/utils/assets_utils.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 import 'package:flutter/services.dart';
 
 class AssetsUtils {
+  AssetsUtils._();
   static Future<void> copyAssetToPath(String key, String path) async {
     final ByteData byteData = await rootBundle.load(key);
     final Uint8List picBytes = byteData.buffer.asUint8List();
