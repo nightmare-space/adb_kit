44776b4a86f4f8802d747711b4aad2ab628bbceb
Nightmare <mengyanshou@gmail.com>
2021-10-04 22:01:24 +0800

    adjust ui

--- lib/app/modules/developer_tool/developer_tool.dart
+++ lib/app/modules/developer_tool/developer_tool.dart
@@ -1,6 +1,15 @@
+import 'package:adb_tool/config/config.dart';
+import 'package:adb_tool/global/instance/global.dart';
+import 'package:adb_tool/global/widget/pop_button.dart';
+import 'package:adb_tool/themes/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:global_repository/global_repository.dart';
-
+import 'package:pseudo_terminal_utils/pseudo_terminal_utils.dart';
+import 'package:termare_pty/termare_pty.dart';
+import 'package:termare_view/termare_view.dart';
 import 'install_apk_page.dart.dart';
 import 'upload_file.dart';
 
@@ -14,6 +23,7 @@ class DeveloperTool extends StatefulWidget {
 }
 
 class _DeveloperToolState extends State<DeveloperTool> {
+  EdgeInsets padding = EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.w);
   @override
   void initState() {
     super.initState();
@@ -22,81 +32,39 @@ class _DeveloperToolState extends State<DeveloperTool> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        brightness: Brightness.light,
-        backgroundColor: Colors.white,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
         centerTitle: true,
         elevation: 0.0,
         title: Text(
-          '开发者工具',
+          widget.serial,
           style: TextStyle(
-            height: 1.0,
             color: Theme.of(context).textTheme.bodyText2.color,
             fontWeight: FontWeight.bold,
           ),
         ),
-        leading: Builder(
-          builder: (_) {
-            return IconButton(
-              icon: const Icon(
-                Icons.arrow_back_ios,
-                color: Colors.black,
-              ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            );
-          },
-        ),
+        leading: const PopButton(),
       ),
       body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
         child: Column(
           children: [
+            Wrap(
+              runSpacing: 8.w,
+              children: [
+                buildOptions(),
+                buildTerminal(),
+              ],
+            ),
             SizedBox(
-              height: Dimens.gap_dp48,
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Dimens.gap_dp12,
-                ),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          const Text(
-                            '设备序列号    ',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black87,
-                            ),
-                          ),
-                          Text(
-                            widget.serial,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              height: 12.w,
             ),
-            _DeveloperItem(
-              serial: widget.serial,
-              title: '显示点按操作反馈',
-              putKey: 'show_touches',
-            ),
-            _DeveloperItem(
-              serial: widget.serial,
-              title: '开启单手模式',
-              putKey: 'handy_mode_state',
+            Wrap(
+              runSpacing: 8.w,
+              children: [
+                uploadFileBox(),
+                uploadFileBox(),
+              ],
             ),
             InkWell(
               onTap: () {
@@ -124,132 +92,229 @@ class _DeveloperToolState extends State<DeveloperTool> {
                 ),
               ),
             ),
-            InkWell(
-              onTap: () {
-                // Navigator.of(context).push<void>(
-                //   MaterialPageRoute(
-                //     builder: (_) {
-                //       return UploadFile(
-                //         serial: widget.serial,
-                //       );
-                //     },
-                //   ),
-                // );
-              },
-              child: SizedBox(
-                height: Dimens.gap_dp48,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: Dimens.gap_dp12,
-                  ),
-                  child: const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      '一键转无线调试',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            _DeveloperItem(
-              serial: widget.serial,
-              title: '显示屏幕指针',
-              putKey: 'pointer_location',
-            ),
-            _OpenRemoteDebug(
-              serial: widget.serial,
-            ),
-            InkWell(
-              onTap: () {
-                Navigator.of(context).push<void>(
-                  MaterialPageRoute(
-                    builder: (_) {
-                      return UploadFile(
-                        serial: widget.serial,
-                      );
-                    },
-                  ),
-                );
-              },
-              child: SizedBox(
-                height: Dimens.gap_dp48,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: Dimens.gap_dp12,
-                  ),
-                  child: const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      '上传文件',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                // showDialog<void>(
-                //   context: context,
-                //   child: DownloadFile(
-                //     serial: widget.serial,
-                //   ),
-                // );
-              },
-              child: SizedBox(
-                height: Dimens.gap_dp48,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: Dimens.gap_dp12,
-                  ),
-                  child: const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      '下载文件',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                Navigator.of(context).push<void>(
-                  MaterialPageRoute(
-                    builder: (_) {
-                      return InstallApkPage(
-                        serial: widget.serial,
-                      );
-                    },
-                  ),
-                );
-              },
-              child: SizedBox(
-                height: Dimens.gap_dp48,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: Dimens.gap_dp12,
-                  ),
-                  child: const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      '向设备安装apk',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
           ],
+        ),
+      ),
+    );
+  }
+
+  ConstrainedBox buildOptions() {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: 414.w,
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.w),
+        child: NiCardButton(
+          margin: EdgeInsets.zero,
+          child: SizedBox(
+            child: Padding(
+              padding: padding,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '常用开关',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      height: 1.0,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  Column(
+                    children: [
+                      _DeveloperItem(
+                        serial: widget.serial,
+                        title: '显示点按操作反馈',
+                        putKey: 'show_touches',
+                      ),
+                      _DeveloperItem(
+                        serial: widget.serial,
+                        title: '开启单手模式',
+                        putKey: 'handy_mode_state',
+                      ),
+                      _DeveloperItem(
+                        serial: widget.serial,
+                        title: '显示屏幕指针',
+                        putKey: 'pointer_location',
+                      ),
+                      _OpenRemoteDebug(
+                        serial: widget.serial,
+                      ),
+                    ],
+                  ),
+                  // Row(
+                  //   children: const <Widget>[
+                  //     Text('标题'),
+                  //     SizedBox(
+                  //       width: 16.0,
+                  //     ),
+                  //     SizedBox(
+                  //       width: 120,
+                  //       child: TextField(
+                  //         decoration: InputDecoration(
+                  //           isDense: true,
+                  //           // border: OutlineInputBorder(),
+                  //           contentPadding: EdgeInsets.symmetric(
+                  //             horizontal: 08.0,
+                  //             vertical: 8.0,
+                  //           ),
+                  //         ),
+                  //       ),
+                  //     )
+                  //   ],
+                  // ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  ConstrainedBox uploadFileBox() {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: 414.w,
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.w),
+        child: NiCardButton(
+          margin: EdgeInsets.zero,
+          child: SizedBox(
+            child: Padding(
+              padding: padding,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '上传文件',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      height: 1.0,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  Container(
+                    height: 100.w,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: AppColors.background,
+                      borderRadius: BorderRadius.circular(4.w),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '拖拽上传',
+                        style: TextStyle(
+                          color: AppColors.fontColor.withOpacity(0.6),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  ConstrainedBox buildTerminal() {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: 414.w,
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.w),
+        child: NiCardButton(
+          margin: EdgeInsets.zero,
+          child: SizedBox(
+            child: Padding(
+              padding: padding,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'SHELL',
+                    style: TextStyle(
+                      height: 1.0,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  LayoutBuilder(
+                    builder: (_, box) {
+                      return ClipRRect(
+                        borderRadius: BorderRadius.circular(4.w),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: AppColors.background,
+                          ),
+                          height: 200,
+                          child: Padding(
+                            padding: EdgeInsets.all(4.w),
+                            child: TermarePty(
+                              pseudoTerminal: TerminalUtil.getShellTerminal(
+                                useIsolate: false,
+                                exec: 'adb',
+                                arguments: ['-s', widget.serial, 'shell'],
+                              ),
+                              controller: TermareController(
+                                fontFamily:
+                                    '${Config.flutterPackage}MenloforPowerline',
+                                theme: TermareStyles.macos.copyWith(
+                                  backgroundColor: AppColors.background,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  // Expanded(
+                  //   child: TermarePty(
+                  //     pseudoTerminal: TerminalUtil.getShellTerminal(useIsolate: false),
+                  //   ),
+                  // ),
+                  // Row(
+                  //   children: const <Widget>[
+                  //     Text('标题'),
+                  //     SizedBox(
+                  //       width: 16.0,
+                  //     ),
+                  //     SizedBox(
+                  //       width: 120,
+                  //       child: TextField(
+                  //         decoration: InputDecoration(
+                  //           isDense: true,
+                  //           // border: OutlineInputBorder(),
+                  //           contentPadding: EdgeInsets.symmetric(
+                  //             horizontal: 08.0,
+                  //             vertical: 8.0,
+                  //           ),
+                  //         ),
+                  //       ),
+                  //     )
+                  //   ],
+                  // ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
@@ -290,9 +355,6 @@ class __OpenRemoteDebugState extends State<_OpenRemoteDebug> {
     return InkWell(
       onTap: () {},
       child: Container(
-        margin: EdgeInsets.symmetric(
-          horizontal: Dimens.gap_dp12,
-        ),
         width: MediaQuery.of(context).size.width,
         height: Dimens.gap_dp48,
         child: Row(
@@ -305,7 +367,7 @@ class __OpenRemoteDebugState extends State<_OpenRemoteDebug> {
                 Row(
                   children: [
                     const Text(
-                      '为设备开启远程调试',
+                      '远程调试',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                       ),
@@ -319,10 +381,11 @@ class __OpenRemoteDebugState extends State<_OpenRemoteDebug> {
                     ),
                   ],
                 ),
-                const Text(
+                Text(
                   '无需root可让设备打开远程调试',
                   style: TextStyle(
                     color: Colors.grey,
+                    fontSize: 12.w,
                   ),
                 ),
               ],
@@ -401,8 +464,8 @@ class __DeveloperItemState extends State<_DeveloperItem> {
       onTap: () {},
       child: Container(
         margin: EdgeInsets.symmetric(
-          horizontal: Dimens.gap_dp12,
-        ),
+            // horizontal: Dimens.gap_dp12,
+            ),
         width: MediaQuery.of(context).size.width,
         height: Dimens.gap_dp48,
         child: Row(
