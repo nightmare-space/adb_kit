71c4e71552b4c9c1651da1b03dff04ef50647723
Nightmare <mengyanshou@gmail.com>
2021-10-04 22:01:59 +0800

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,7 +1,8 @@
-version='1.1.3'
+version='1.1.4'
 app_name='AdbTool'
 target=root@$server:/home/nightmare/YanTool/resources/AdbTool
-if [ -f "$app_name\_macOS.tar" ]; then
-    rsync -v "$app_name\_macOS.tar" "$target/$app_name\_$version\_macOS.tar"
+if [ -f $app_name'_macOS.tar' ]; then
+    rsync -v $app_name'_macOS.tar' $target'/'$app_name'_'$version'_macOS'.tar
 fi
-rsync -v build/app/outputs/apk/release/app-release.apk "$target/$app_name\_$version\_Android_arm64.apk"
+echo $target'/'$app_name'_'$version_macOS.tar
+#rsync -v build/app/outputs/apk/release/app-release.apk "$target/$app_name\_$version\_Android_arm64.apk"
