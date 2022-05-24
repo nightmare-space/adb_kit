61fe6f46fe3e1e001f734bab66f8017738758ece
mengyanshou <mengyanshou@gmail.com>
2022-05-24 16:41:54 +0800

    fix: some bug

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:global_repository/global_repository.dart';
 import 'package:app_manager/app_manager.dart' as am;
+import 'package:path_provider/path_provider.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'app/modules/home/views/adaptive_view.dart';
 import 'app_entrypoint.dart';
@@ -23,9 +24,7 @@ bool useNativeShell = false;
 Future<void> main() async {
   // Log.d(StackTrace.current);
   // 初始化运行时环境
-  if (!GetPlatform.isIOS) {
-    RuntimeEnvir.initEnvirWithPackageName(Config.packageName);
-  }
+
   PluginManager.instance.register(DashboardPlugin());
   if (!GetPlatform.isWindows) {
     PluginManager.instance.register(AppStarterPlugin());
@@ -51,8 +50,15 @@ void runADBClient({Color primary}) {
     seed = primary;
   }
   runZonedGuarded<void>(
-    () {
+    () async {
       WidgetsFlutterBinding.ensureInitialized();
+      if (!GetPlatform.isIOS) {
+        final dir = (await getApplicationSupportDirectory()).path;
+        RuntimeEnvir.initEnvirWithPackageName(
+          Config.packageName,
+          appSupportDirectory: dir,
+        );
+      }
       runApp(const MaterialAppWrapper());
     },
     (error, stackTrace) {
--- scripts/patch_executable.sh
+++ scripts/patch_executable.sh
@@ -1,2 +1,4 @@
-mkdir -p "./build/macos/Build/Products/Release/ADB TOOL.app/Contents/MacOS/data/usr/bin"
-cp ./executable/macos/adb "./build/macos/Build/Products/Release/ADB TOOL.app/Contents/MacOS/data/usr/bin/"
+bin="./build/macos/Build/Products/Release/ADB TOOL.app/Contents/MacOS/data/usr/bin"
+mkdir -p $bin
+cp ./res/macos/adb "$bin/"
+chmod +x "$bin/adb"
