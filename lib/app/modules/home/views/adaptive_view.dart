7c916054de77877e4061861e5e3a7db63bfc684f
nightmare <mengyanshou@gmail.com>
2022-08-17 18:42:46 +0800

    fix: window manager bug

--- lib/app/modules/home/views/adaptive_view.dart
+++ lib/app/modules/home/views/adaptive_view.dart
@@ -34,6 +34,7 @@ class AdbTool extends StatefulWidget {
 class _AdbToolState extends State<AdbTool> with WidgetsBindingObserver {
   bool dialogIsShow = false;
   ConfigController configController = Get.find();
+  bool isFull = false;
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
@@ -90,39 +91,10 @@ class _AdbToolState extends State<AdbTool> with WidgetsBindingObserver {
       value: Theme.of(context).brightness == Brightness.dark
           ? OverlayStyle.light
           : OverlayStyle.dark,
-      child: Column(
+      child: Stack(
         children: [
-          Center(
-            child: Container(
-              color: configController.theme.colorScheme.background,
-              width: double.infinity,
-              height: 24,
-              child: DragToMoveArea(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    WindowCaptionButton.minimize(
-                      onPressed: () {
-                        windowManager.minimize();
-                      },
-                    ),
-                    WindowCaptionButton.maximize(
-                      onPressed: () {
-                        windowManager.maximize();
-                      },
-                    ),
-                    WindowCaptionButton.close(
-                      onPressed: () {
-                        windowManager.close();
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Builder(builder: (context) {
+          Builder(
+            builder: (context) {
               if (ResponsiveWrapper.of(context).isDesktop) {
                 return Scaffold(
                   body: Row(
@@ -145,23 +117,21 @@ class _AdbToolState extends State<AdbTool> with WidgetsBindingObserver {
                             .withOpacity(0.4),
                       ),
                       Expanded(
-                        child: MacSafeArea(
-                          child: PageTransitionSwitcher(
-                            transitionBuilder: (
-                              Widget child,
-                              Animation<double> animation,
-                              Animation<double> secondaryAnimation,
-                            ) {
-                              return FadeThroughTransition(
-                                animation: animation,
-                                secondaryAnimation: secondaryAnimation,
-                                fillColor: Colors.transparent,
-                                child: child,
-                              );
-                            },
-                            duration: const Duration(milliseconds: 300),
-                            child: Global().page,
-                          ),
+                        child: PageTransitionSwitcher(
+                          transitionBuilder: (
+                            Widget child,
+                            Animation<double> animation,
+                            Animation<double> secondaryAnimation,
+                          ) {
+                            return FadeThroughTransition(
+                              animation: animation,
+                              secondaryAnimation: secondaryAnimation,
+                              fillColor: Colors.transparent,
+                              child: child,
+                            );
+                          },
+                          duration: const Duration(milliseconds: 300),
+                          child: Global().page,
                         ),
                       ),
                     ],
@@ -244,7 +214,42 @@ class _AdbToolState extends State<AdbTool> with WidgetsBindingObserver {
                 );
               }
               return const SizedBox();
-            }),
+            },
+          ),
+          Container(
+            color: configController.theme.colorScheme.background,
+            width: double.infinity,
+            height: 24,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                WindowCaptionButton.minimize(
+                  onPressed: () {
+                    windowManager.minimize();
+                  },
+                ),
+                isFull
+                    ? WindowCaptionButton.unmaximize(
+                        onPressed: () {
+                          isFull = false;
+                          setState(() {});
+                          windowManager.unmaximize();
+                        },
+                      )
+                    : WindowCaptionButton.maximize(
+                        onPressed: () {
+                          isFull = true;
+                          setState(() {});
+                          windowManager.maximize();
+                        },
+                      ),
+                WindowCaptionButton.close(
+                  onPressed: () {
+                    windowManager.close();
+                  },
+                ),
+              ],
+            ),
           ),
         ],
       ),
