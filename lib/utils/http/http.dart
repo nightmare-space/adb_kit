288e6700bb4a8616d62503a62dc0ffb480308011
nightmare <mengyanshou@gmail.com>
2024-09-02 17:13:38 +0800

    Remove unused file

--- lib/utils/http/http.dart
+++
@@ -1,9 +0,0 @@
-library http;
-
-import 'dart:io';
-import 'package:dio/dio.dart';
-part 'src/exception.dart';
-part 'src/dio_utils.dart';
-part 'src/interceptors.dart';
-
-Dio? httpInstance = DioUtils.getInstance();
