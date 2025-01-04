3ca822da2fdc686dd543f5e65eb71794694e1edf
nightmare <mengyanshou@gmail.com>
2025-01-04 21:52:36 +0800

    fix: History address problem

--- lib/app/controller/history_controller.dart
+++ lib/app/controller/history_controller.dart
@@ -118,6 +118,8 @@ class HistoryController extends GetxController {
       );
       preData.connectTime = data.connectTime;
       preData.name = data.name;
+      preData.address = data.address;
+      preData.port = data.port;
       // Log.i('${data.name} already exist in history');
     } catch (e) {
       adbHistorys.data.add(data);
