4409bd52daca431840b76d9125246c6e5409105a
nightmare <mengyanshou@gmail.com>
2025-01-06 20:11:50 +0800

    Create test_controller.dart

---
+++ lib/test_controller.dart
@@ -0,0 +1,10 @@
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:get/get.dart';
+
+class TestController extends GetxController {
+  Uint8List imageBytes = Uint8List(0);
+
+  Size size = const Size(0, 0);
+}
