3cf4e687dee3ecf17bc21ab500c64d22b4eb4688
mengyanshou <mengyanshou@gmail.com>
2025-03-14 13:17:31 +0800

    I18n

--- packages/plugins/lib/plugins.dart
+++ packages/plugins/lib/plugins.dart
@@ -6,23 +6,29 @@ export 'plugins/file_manager/file_manager.dart';
 export 'generated/l10n.dart';
 export 'widget/i18n_wrapper.dart';
 import 'package:adb_kit/adb_kit.dart';
-import 'package:android_tool/android_tool.dart';
+import 'package:android_tool/android_tool.dart' as at;
 import 'generated/intl/messages_en.dart' as en;
 import 'generated/intl/messages_zh_CN.dart' as zh_cn;
 import 'plugins.dart';
 
-Map<String, dynamic> en_message = en.messages.messages;
-Map<String, dynamic> zh_cn_messages = zh_cn.messages.messages;
+Map<String, Function> en_message = {
+  ...en.messages.messages,
+  ...at.en_message,
+};
+Map<String, Function> zh_cn_messages = {
+  ...zh_cn.messages.messages,
+  ...at.zh_cn_messages,
+};
 
 void registerADBPlugin() {
   Future.delayed(const Duration(milliseconds: 1000), () {
     PluginManager.instance
       ..registerADBPlugin(DashboardPlugin())
       ..registerADBPlugin(FilePlugin())
-      ..registerADBPlugin(AppStarterPlugin())
-      ..registerADBPlugin(AppManagerPlugin())
-      ..registerADBPlugin(DeviceInfoPlugin())
-      ..registerADBPlugin(ProcessPlugin())
-      ..registerADBPlugin(TaskManagerPlugin());
+      ..registerADBPlugin(at.AppStarterPlugin())
+      ..registerADBPlugin(at.AppManagerPlugin())
+      ..registerADBPlugin(at.DeviceInfoPlugin())
+      ..registerADBPlugin(at.ProcessPlugin())
+      ..registerADBPlugin(at.TaskManagerPlugin());
   });
 }
--- packages/plugins/lib/widget/i18n_wrapper.dart
+++ packages/plugins/lib/widget/i18n_wrapper.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:android_tool/android_tool.dart';
 import '../generated/l10n.dart';
 
 /// ADB KIT I18n Wrapper
@@ -19,6 +20,7 @@ class _AKI18nWrapperState extends State<AKI18nWrapper> {
       locale: Localizations.localeOf(context),
       delegates: const [
         P.delegate,
+        IA.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
